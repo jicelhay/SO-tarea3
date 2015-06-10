69f8d5997383b9005f8e3600fa20f586a23dad4f
juanito <juan.celhay@gmail.com>
2015-06-10 20:45:06 -0300

    corregido, con informe semi-hecho

--- simple_server.c
+++ simple_server.c
@@ -13,6 +13,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
 
 /**
  * Funcion para desplegar los errores
@@ -24,6 +26,108 @@ void error(const char *msg)
 }
 
 
+//Creamos el socket parecido al chat
+int create_tcp_socket();
+
+//Se busca la IP del host
+char *get_ip(char *host);
+
+//Se arma el request HTTP
+char *build_get_query(char *host, char *page);
+
+// Un método que le dice al usuario como usar el programa
+void usage();
+
+
+// Se define un host por defecto-----
+#define HOST "www.uc.cl"
+#define PAGE "/"
+#define PORT 80
+#define USERAGENT "HTMLGET 1.0"
+//---------------------------
+
+
+
+//Le digo al usuario como usar mi programa
+
+
+void usage()
+{
+    fprintf(stderr, "USO: htmlget host [recurso solicitado]\n\
+            \thost: El host del sitio ejemplo: www.uc.cl\n\
+            \trecurso solicitado: el recurso que quiero ejemplo:index.html, default: /\n");
+}
+
+
+// Creo el socket
+int create_tcp_socket()
+{
+    int sock;
+    //Acá se setea el socket y se buscan errores
+    if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
+        perror("No se puede crear  el socket");
+        exit(0);
+    }
+    return sock;
+}
+
+
+//Obtendo la IP
+char *get_ip(char *host)
+{
+    // hostent libería netdb
+    struct hostent *hent;
+    
+    //El largo de la IP
+    int iplen = 15; //XXX.XXX.XXX.XXX
+    
+    //Se crea un string del largo de la ip que va a ser lo que devolveremos
+    char *ip2 = (char *)malloc(iplen+1);
+    
+    //Seteamos un trozo de memoria
+    memset(ip2, 0, iplen+1);
+    
+    // Se crea el "objeto hostent hent"
+    hent = gethostbyname(host);
+    
+    // magia netdb que setea la ip que obtuvo del host
+    inet_ntop(AF_INET, (void *)hent->h_addr_list[0], ip2, iplen);
+    
+    return ip2;
+}
+
+
+
+
+
+//Armo la solicitud -Podría modificarse...
+char *build_get_query(char *host, char *page)
+{
+    //El string de la query
+    char *query;
+    //La página solicitada
+    char *getpage = page;
+    
+    //La solicitud GET, esto lo tienene que modificar porque tiene que llegar desde el cliente a su proxy
+    char *tpl = "HEAD /%s HTTP/1.0\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n";
+    
+    // Qué pasará si pongo HEAD???????!
+    
+    if(getpage[0] == '/'){
+        getpage = getpage + 1;
+        fprintf(stderr," \"/\",  %s %s\n", page, getpage);
+    }
+    
+    //Creamos el string query vacío
+    query = (char *)malloc(strlen(host)+strlen(getpage)+strlen(USERAGENT)+strlen(tpl)-5);
+    
+    //Se setea acá:
+    sprintf(query, tpl, getpage, host, USERAGENT);
+    return query;
+}
+
+
 /**
  * Funcion principal, se llama indicando el puerto de escucha
  */
@@ -31,6 +135,15 @@ int main(int argc, char *argv[])
 {
 	
 	// Definimos estructuras necesarias
+    //necesarioas para el request
+    struct sockaddr_in *remote;
+    int sock;
+    int tmpres;
+    char *ip2;
+    char *get2;
+    char buf[BUFSIZ+1];
+    char *host;
+    char *page;
 
 	//Descriptor del servidor
 	int server_socket_descriptor;
@@ -158,7 +271,7 @@ int main(int argc, char *argv[])
 
 	//ver si el ip está en el archivo de permitidos
 
-	int c;
+		int c;
 	FILE *file;
 	file = fopen("ips", "r");
  	char line[50];
@@ -175,17 +288,16 @@ int main(int argc, char *argv[])
 				   }
     }
 		  }
-	
 
 	if(permited){
 
 	printf("IP permitido, se realizara petición\n");
 
 	//ver si el archivo está en cache
-	char *fname = malloc (strlen ("cache\\") + strlen(IP) + strlen(".txt")+1);
+	char *fname = malloc (strlen ("cache\\") + strlen(IP));
+    	char *fnameaux= IP;
 	strcpy (fname, "cache/");
 	strcat (fname,IP);
-	//strcat (fname, ".txt");
 
 	 FILE *f;
 	f= fopen(fname, "r");
@@ -222,7 +334,154 @@ int main(int argc, char *argv[])
 		printf( "no se encontro el archivo: %s hay que hacer http request\n",fname);
 
 		//en este caso no existe cache y tenemos que hacer request, ---> INSERTAR REQUEST AQUI.
-		
+        
+        //if(argc == 1)
+        //{
+        //  usage();
+        //exit(0);
+        //}
+        //char *variable1="./htmlget ";
+        //host = (char *)malloc(strlen(variable1)+strlen(fname));
+        
+        host=fnameaux;
+        
+        if(argc > 2)
+        {
+            page = argv[2];
+        }
+        else
+        {
+            page = PAGE;
+        }
+        printf("logramos pasar. ahora vamos a crear el tcpsocket\n");
+        sock = create_tcp_socket();
+        printf("vamos a ver la ip del servidor\n");
+        ip2 = get_ip(host);
+        printf("logramos conseguir la ip\n");
+        fprintf(stderr, "La IP del host es: %s\n", ip2);
+        
+        remote = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in *));
+        
+        
+        remote->sin_family = AF_INET;
+        tmpres = inet_pton(AF_INET, ip2, (void *)(&(remote->sin_addr.s_addr)));
+        
+        
+        //Según el valor del tmpres le digo al usuario que es lo que pasa ------------
+        if( tmpres < 0)
+        {
+            perror("No se puede setear remote->sin_addr.s_addr");
+            exit(1);
+        }else if(tmpres == 0)
+        {
+            fprintf(stderr, "%s No es un dirección válida\n", ip2);
+            exit(1);
+        }
+        //----------------------------
+        
+        
+        // Lo mismo con el puerto
+        remote->sin_port = htons(PORT);
+        
+        if(connect(sock, (struct sockaddr *)remote, sizeof(struct sockaddr)) < 0){
+            perror("No se pudo conectar");
+            exit(0);
+        }
+        
+        //Acá le decimos al usuario cuál es su solicitud con el método build query
+        get2 = build_get_query(host, page);
+        fprintf(stderr,
+                "Query is:\n<<SU REQUEST TIENE ESTOS DATOS>>\n%s<<FIN>>\n", get2);
+        
+        
+        
+        
+        
+        //Enviamos la solicitud al servidor:
+        
+        //Comenzamos a mandar el string de la query
+        int sent = 0;
+        
+        while(sent < strlen(get2))
+        {
+            
+            
+            tmpres = send(sock, get2+sent, strlen(get2)-sent, 0);
+            
+            
+            //Nuevamente revisamos algún error:
+            if(tmpres == -1)
+                
+            {
+                perror("No se puede mandar la solicitud");
+                exit(1);
+            }
+            sent += tmpres;
+        }
+    
+        
+        memset(buf, 0, sizeof(buf));
+        int htmlstart = 0;
+        char * htmlcontent;
+        char output[10000];
+        strcpy(output,"");
+        
+        //importante
+        
+        
+        while((tmpres = recv(sock, buf, BUFSIZ, 0)) > 0)
+        {
+            if(htmlstart == 0)
+            {
+                //Si lo ocupan así obtendrán solo el archivo
+                //htmlcontent = strstr(buf, "\r\n\r\n");
+                
+                //Si lo ocupan así tendrán en HEAD+ El resto del archivo
+                htmlcontent = strstr(buf, "");
+                strcat(output,buf);
+                
+                if(htmlcontent != NULL)
+                {
+                    htmlstart = 1;
+                    htmlcontent += 4;
+                }
+            }
+            else
+            {
+                htmlcontent = buf;
+                strcat (output,buf);
+            }
+            
+            if(htmlstart)
+            {
+                //  printf(buf);
+                fprintf(stdout, htmlcontent);
+            }
+            
+            memset(buf, 0, tmpres);
+        }
+        
+        
+        if(tmpres < 0)
+        {
+            perror("Error al recibir el contenido");
+        }
+        
+        //Se libera el socket (librería sockets)
+        free(get2);
+        free(remote);
+        free(ip2);
+        close(sock);
+        
+        
+        
+        
+        
+
+        
+        
+        
+       
 		//creamos el archivo con fname y contenido de request.
 		FILE *f_request = fopen(fname, "wb");
 		if (f_request == NULL)
@@ -231,7 +490,7 @@ int main(int argc, char *argv[])
 		}	
 		
 		printf("se escribira en nuevo archivo\n");
-		 char *text = "Esto deberia ser el request";
+		 char *text = output;
 		fprintf(f_request, text);
 		n = write(client_socket_descriptor,text,strlen(text));
 
